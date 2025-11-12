fa2ef1ec44eb27e9cdee10c5c7ad9341c5492863
Lucas Duarte <lucasduartedev@gmail.com>
2025-11-12 16:28:53 -0300

    Início do projeto

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Prática de testes em Java com JUnit
+
+## Objetivo
+- Este projeto serve apenas para aplicar conhecimentos em testes em Java usando a biblioteca JUnit. Conterá diversos tipos de mini projetos para aplicar lógica de programação e testes.
+
+## Tecnologias
+- Java 17
+- Projeto Spring Boot
---
+++ src/main/java/com/fundamentos/FundamentosTestesApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosTestesApplication.class, args);
+	}
+
+}
